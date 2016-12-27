5df4bc16abf4a094f599e51b674251012c5bce2a
root <root@ip-172-31-30-227.us-west-2.compute.internal>
2016-12-27 10:23:52 -0500

    Removed configuration fronm Git Web Hook

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -3,6 +3,5 @@ package oata;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello NEW World AGAIN");
-        System.out.println("For Git Webhook ");
     }
 }
