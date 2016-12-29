6c45902c24c78eeb797ec1bab1f375c9ca7bdccd
skimeer <skimeer@gmail.com>
2016-12-29 11:18:11 -0500

    Test code pipeline 2

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -2,6 +2,6 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome in New Year 2017");
+        System.out.println("Welcome in New Year 2017 to you");
     }
 }
