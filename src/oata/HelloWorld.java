997ffb83b3d4dbac544241850ff6836c0a848d6c
skimeer <skimeer@gmail.com>
2016-12-29 11:12:46 -0500

    Testing code pipline

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -2,6 +2,6 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello NEW World AGAIN");
+        System.out.println("Welcome in New Year 2017");
     }
 }
